a449987dc93b5648a73fdbd46dd8ca813b63e6f2
Roberto Bertelli <robbybertelli@gmail.com>
2023-06-05 23:17:00 +0200

    Formatted
    Signed-off-by: Roberto Bertelli <robbybertelli@gmail.com>

--- vinted_label.py
+++ vinted_label.py
@@ -9,10 +9,11 @@ import re
 from wand.image import Image
 from wand.color import Color
 
-
-attachments = {} 
-credentials = google.oauth2.credentials.Credentials.from_authorized_user_file('credentials.json', ['https://www.googleapis.com/auth/gmail.readonly'])
+attachments = {}
+credentials = google.oauth2.credentials.Credentials.from_authorized_user_file(
+    'credentials.json', ['https://www.googleapis.com/auth/gmail.readonly'])
 service = build('gmail', 'v1', credentials=credentials)
+
 
 def htmlb64_to_plain(data):
     bytes = base64.urlsafe_b64decode(data)
@@ -20,28 +21,36 @@ def htmlb64_to_plain(data):
     soup = BeautifulSoup(body_text, 'html.parser')
     return html2text.html2text(str(soup))
 
+
 def download_attachments(part, msg_id):
     body = part['body']
     filename = os.path.join("attachments", part['filename'])
     attachment = service.users().messages().attachments().get(
-        userId='me', messageId=msg_id, id=body['attachmentId']
-    ).execute()
+        userId='me', messageId=msg_id, id=body['attachmentId']).execute()
     file_data = attachment['data']
     file_content = base64.urlsafe_b64decode(file_data)
     with open(filename, 'wb') as f:
         f.write(file_content)
-        logging.info(f"successfully downloaded attachment with name: {filename}")
+        logging.info(
+            f"successfully downloaded attachment with name: {filename}")
     return filename
 
+
 def download_all_attachments():
-    messages = service.users().messages().list(userId='me', labelIds = ['Label_5694856354124115227'], q="is:unread").execute()
+    messages = service.users().messages().list(userId='me',
+                                               labelIds=[
+                                                   'Label_5694856354124115227'
+                                               ],
+                                               q="is:unread").execute()
     if 'messages' not in messages:
         logging.info("no messages found")
         return
-    
+
     attachments = {}
     for message in messages['messages']:
-        msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
+        msg = service.users().messages().get(userId='me',
+                                             id=message['id'],
+                                             format='full').execute()
         logging.info(f"managing email with snippet:{msg['snippet']}")
 
         transaction_n = ""
@@ -51,8 +60,13 @@ def download_all_attachments():
             data = part['body']
             if 'data' in data:
                 msg_txt = htmlb64_to_plain(data['data'])
-                match = re.search(pattern=r"^\*\*N\. transazione:\*\* \| (\d*).*", string=msg_txt, flags=re.MULTILINE)
+                match = re.search(
+                    pattern=r"^\*\*N\. transazione:\*\* \| (\d*).*",
+                    string=msg_txt,
+                    flags=re.MULTILINE)
                 if not match:
+                    logging.critical(
+                        "transaction number not found in email body, exiting")
                     exit(1)
                 transaction_n = match.group(1)
             if 'filename' in part:
@@ -73,6 +87,7 @@ def download_all_attachments():
 
     return attachments
 
+
 def main():
     attachments = download_all_attachments()
     for keys, values in attachments.items():
@@ -89,6 +104,7 @@ def main():
 
             # Save the trimmed image
             img.save(filename='output.jpg')
+
 
 if __name__ == "__main__":
     main()
